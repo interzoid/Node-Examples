a99e3166188a59f6b2e772aa94711d9c326de400
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-08-30 13:32:26 -0700

    Create GetAddressMatchSimKey.js

---
+++ GetAddressMatchSimKey.js
@@ -0,0 +1,21 @@
+// Get API key at www.interzoid.com/register
+// npm install request --save (if necessary to install request module)
+
+const request = require('request');
+
+const options = {
+  method: 'GET',
+  url: 'https://api.interzoid.com/getaddressmatchadvanced',
+  qs: {address: '100 East Main St Ste #101', algorithm: 'narrow'},
+  headers: {
+    'content-type': 'application/json',
+    'x-api-key': 'YOURAPIKEY',
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
